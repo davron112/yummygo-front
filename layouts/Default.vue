32f23c7e463c615298e3d7dfd5f5c53e70635fc2
Davron <davron211@gmail.com>
2022-03-13 23:40:33 +0500

    fix

---
+++ layouts/Default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <NavbarMenu />
+    <div>
+      <nuxt />
+    </div>
+    <FooterSection />
+  </div>
+</template>
+
+<script>
+import FooterSection from '~/components/Footer'
+import NavbarMenu from '~/components/NavbarMenu'
+export default {
+  name: 'Default',
+  components: { NavbarMenu, FooterSection },
+}
+</script>
